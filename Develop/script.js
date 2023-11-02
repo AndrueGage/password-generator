804ab36390a4f4322b4d206fd5729caac2ece50e
AndrueGage <trainer.andrue@gmail.com>
2023-11-01 23:50:56 -0500

    Added var characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
+// var for characters (upper,lower,special, numbers)
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numCharacter = "0123456789"
+var specialCharacter = "!@#$%^&*()-_=+][{}|<.>/?~`"
+// character length 8 to 128
+// browser alert prompts 
+// validated inputs from prompts
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
