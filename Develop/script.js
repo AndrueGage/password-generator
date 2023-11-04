5bef8731833b89c29627fedc9e5f2dd759e751f9
AndrueGage <trainer.andrue@gmail.com>
2023-11-03 22:04:10 -0500

    added quality README and finished script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,62 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
-// var for characters (upper,lower,special, numbers)
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numCharacter = "0123456789"
 var specialCharacter = "!@#$%^&*()-_=+][{}|<.>/?~`"
-// character length 8 to 128
-// browser alert prompts 
-// validated inputs from prompts
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function selectOptions() {
+  var passwordOptionsCheck = true
+  var selectedChars = ""
+  var passwordLength = window.prompt("How long would you like your password? Choose a number between 8 and 128.")
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You did not pick a correct number.");
+    return;
+  }
+  var confirmLower = confirm("Do you want lowercase letters in this password?")
 
-// Write password to the #password input
+  if (confirmLower) {
+    selectedChars = selectedChars + lowerCase;
+    passwordOptionsCheck = false; 
+  }
+  var confirmUpper = confirm("Do you want uppercase letters in this password? ")
+
+  if (confirmUpper) {
+    selectedChars = selectedChars + upperCase;
+    passwordOptionsCheck = false; 
+  }
+  var confirmNum = confirm("Do you want numbers in this password?")
+
+  if (confirmNum) {
+    selectedChars = selectedChars + numCharacter;
+    passwordOptionsCheck = false; 
+  }
+  var confirmSpec = confirm("Do you want special characters in this password?")
+
+  if (confirmSpec) {
+    selectedChars = selectedChars + specialCharacter;
+    passwordOptionsCheck = false;
+  }
+
+  if (passwordOptionsCheck) {
+    alert("You did not choose any options.");
+    return;
+  }
+  generate(passwordLength, selectedChars);
+}
+
+function generate(length, chars) {
+  var finalPassword = ""
+  for (var i = 0, n = chars.length; i < length; i++) {
+   finalPassword += chars.charAt(Math.floor(Math.random() * n));
+  }
+  writePassword(finalPassword)
+}
+
+generateBtn.addEventListener("click", selectOptions);
+
 function writePassword(password) {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# Password Generator Starter Code
+# Password Generator
+This is a random password generator made with JavaScript! Follow the prompts that the window displays after clicking the generate button for your new password. Choose how many characters you would like from 8 to 128 characters. Then follow the prompts to pick if you would like lowercase, uppercase, numbers, and/or special characters!
 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+## Table of Contents
+
+- [Preview](#preview)
+- [License](#license)
+- [Contact](#contact)
+  
+## Preview
+
+## License
+
+This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit/). You are free to use, modify, and distribute this project as long as you provide proper attribution and include the original license in your distribution.
+
+## Contact
+
+You can contact me via [trainer.andrue@gmail.com](mailto:trainer.andrue@gmail.com)
+Check out my [github](https://github.com/AndrueGage) for more projects. 
